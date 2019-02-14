218aa84b50b8b974e8fe9749d0178fb647dfeab1
malexsan <alexandru.munt@gmail.com>
2019-02-14 16:45:17 +0200

    feat(slide): create basic structure of slides

--- presentation/index.js
+++ presentation/index.js
@@ -31,14 +31,69 @@ const theme = createTheme(
   {
     primary: 'white',
     secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE'
+    quaternary: '#03A9FC',
+    tertiary: '#CECECE'
   },
   {
     primary: 'Montserrat',
     secondary: 'Helvetica'
   }
 );
+
+class RevealingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 0
+    };
+    this._nextStep = this._nextStep.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this._nextStep);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this._nextStep);
+  }
+  _nextStep(e) {
+    if (e.which === 40) {
+      // arrowDown
+      this.setState((p) => ({
+        step: p.step + 1
+      }));
+    }
+  }
+  render() {
+    const { step } = this.state;
+    const { caps, items, title } = this.props // eslint-disable-line
+    return (
+      <Slide bgColor="quaternary" textColor="primary">
+        <Heading size={4} textColor="primary" caps={caps}>
+          {title}
+        </Heading>
+        <List textAlign="center">
+          {items.slice(0, step).map((item, index) => (
+            <ListItem style={{ listStyle: 'none' }} key={index}>
+              {item.title}
+            </ListItem>
+          ))}
+        </List>
+      </Slide>
+    );
+  }
+}
+
+const protocols = [
+  { title: 'Internet Protocol (IP)' },
+  { title: 'Transmission Control Protocol (TCP)' }
+];
+
+const www = [
+  { title: 'Web Resource' },
+  { title: 'Web Browsers' },
+  { title: 'Uniform Resource Locators (URL)' }
+];
+
+const basics = [{ title: 'HTML' }, { title: 'CSS' }, { title: 'Javascript' }];
 
 export default class Presentation extends React.Component {
   render() {
@@ -48,51 +103,37 @@ export default class Presentation extends React.Component {
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+        <Slide transition={['zoom']} bgColor="quaternary">
+          <Heading size={3} caps lineHeight={1} textColor="primary">
+            the basics of web development
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" fit bold>
-            open the presentation/index.js file to get started
-          </Text>
         </Slide>
-        <Slide bgColor="secondary">
-          <Image src={images.formidagon} width={800} />
-        </Slide>
+
+        <Slide
+          bgColor="quaternary"
+          bgImage="https://cdn-images-1.medium.com/max/2000/1*oohdOCH3vjWP914Cc_1VJw.jpeg"
+        />
+
+        <Slide
+          bgColor="quaternary"
+          bgImage="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fsearchengineland.com%2Ffigz%2Fwp-content%2Fseloads%2F2015%2F12%2Fcomputer-network-laptops2-ss-1920.jpg"
+        />
+
+        <RevealingList title="Protocols of the internet" items={protocols} />
+
+        <RevealingList title="World Wide Web" items={www} />
+
+        <RevealingList title="The web page" items={basics} />
+
         <Slide transition={['fade']} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>
             Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
           </Heading>
           <Text size={6} textColor="secondary">
             Standard text
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
+
         <Slide transition={['fade']} bgColor="secondary" textColor="primary">
           <BlockQuote>
             <Quote>Example Quote</Quote>
